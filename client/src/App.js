2d27ebe0ad0e3b90c5d25c3c7183cc7f7cddb8db
Petya <p_babukova@abv.bg>
2021-03-14 22:57:58 +0200

    aside menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import Header from "./components/Header";
+import Menu from "./components/Menu"
+import Main from "./components/Main"
 import style from './App.module.css';
 
 function App() {
-  return (
-    <div className={style.app}>
-        <Header />
-    </div>
-  );
+    return (
+        <div className={style.app}>
+            <Header />
+
+            <div className={style.container}>
+                <Menu />
+
+                <Main />
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,24 @@
+import './Main.css';
+
+const Main = () => {
+    return (
+        <main classname="main-container">
+            <h1>Sooooooome Heading</h1>
+            <p>some posts</p>
+
+        </main>
+    );
+}
+
+export default Main;
+// <main className="main"></main>
+
+// import {Fragment} from 'react'
+    // <Fragment>
+    //         <h1>Sooooooome heading</h1>
+    //         <p>some posts</p>
+
+    //     </Fragment>
+
+    // <> </> - empty tag is equal to <Fragment>
+
